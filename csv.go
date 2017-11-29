518ec33fde5ca37ea95cb62bac002771081dd245
Eliran Bivas <eliran.bivas@gmail.com>
2017-11-29 15:37:53 +0200

    Add unmarshal by index

--- csv.go
+++ csv.go
@@ -61,3 +61,48 @@ func Unmarshal(reader *csv.Reader, v interface{}) error {
 	}
 	return nil
 }
+
+func UnmarshalFieldsByIndex(reader *csv.Reader, v interface{}, indices ...int) error {
+	if len(indices) == 0 {
+		return Unmarshal(reader, v)
+	}
+	record, err := reader.Read()
+	if err != nil {
+		return err
+	}
+	s := reflect.ValueOf(v).Elem()
+	if s.NumField() != len(record) {
+		return &FieldMismatch{s.NumField(), len(record)}
+	}
+	if len(indices) > len(record) {
+		return nil
+	}
+	for _, i := range indices {
+		f := s.Field(i)
+		switch f.Kind() {
+		case reflect.String:
+			f.SetString(record[i])
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			ival, err := strconv.ParseInt(record[i], 10, 64)
+			if err != nil {
+				return err
+			}
+			f.SetInt(ival)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			ival, err := strconv.ParseUint(record[i], 10, 64)
+			if err != nil {
+				return err
+			}
+			f.SetUint(ival)
+		case reflect.Float32, reflect.Float64:
+			ival, err := strconv.ParseFloat(record[i], 64)
+			if err != nil {
+				return err
+			}
+			f.SetFloat(ival)
+		default:
+			return &UnsupportedType{f.Kind()}
+		}
+	}
+	return nil
+}
